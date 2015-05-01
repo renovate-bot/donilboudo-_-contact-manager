e9721312110813fe2c91c470f37985dc34b6d678
Fabrice ILBOUDO <ilboudofabrice@yahoo.fr>
2015-05-01 19:29:49 -0400

    Update, Remove contact
    Adding some method to remove contact of the collection and also update
    the contact. For the moment, the update don't yet work well

--- js/main.js
+++ js/main.js
@@ -14,17 +14,37 @@ var ContactsList = Backbone.Collection.extend({
 //instance of collection
 var contact_list = new ContactsList();
 
-//View to show items collection
+//View to show contacts 
 var ContactListView = Backbone.View.extend({
-  template: _.template('<tr>\
+  template: _.template("<tr id=contact>\
                        <td><%=lastname%></td>\
                        <td><%=firstname%></td>\
                        <td><%=phone%></td>\
-                       </tr>'),
+                       <td><input type='button' id='update_button' value='Update' /></td>\
+                       <td><input type='button' id='remove_button' value='Remove' /></td>\
+                       </tr>"),
 
-  render: function(model) {
+   events: {
+            "click #update_button": "updateContact",
+            "click #remove_button": "removeContact"
+        },
+
+  render: function(model, collection) {
     this.$el.html(this.template(this.model.toJSON()));
     return this;
+  },
+
+  updateContact: function () {
+    var contact = this.model.toJSON();
+     console.log(contact);
+    $("#last-name").val(contact.lastname);
+    $("#first-name").val(contact.firstname);
+    $("#phone").val(contact.phone);
+  },
+
+  removeContact: function () {
+    contact_list.remove(this.model);
+    this.$('#contact').remove();
   }
 });
 
@@ -35,7 +55,7 @@ var MainView = Backbone.View.extend({
                        <tr><td>Last Name</td><td><input type='text' id='last-name' /></td></tr>\
                        <tr><td>First Name</td><td><input type='text' id='first-name' /></td></tr>\
                        <tr><td>Phone</td><td><input type='text' id='phone' /></td></tr>\
-                        <tr><td></td><td><input type='button' id='add_button' value='Add' /><input type='button' id='reset_button' value='Reset' /></td></tr>\
+                        <tr><td></td><td><input type='button' id='add_button' value='Save' /><input type='button' id='reset_button' value='Reset' /></td></tr>\
                        </table><br/><br/>\
                        <h4>Contacts</h4>\
                        <table id='contactList'></table>"),
@@ -46,7 +66,7 @@ var MainView = Backbone.View.extend({
 
   events: {
             "click #add_button": "addContact",
-            "click #reset_button": "removeItems"
+            "click #reset_button": 'removeItems'
         },
 
   addContact: function(event) {
@@ -54,19 +74,17 @@ var MainView = Backbone.View.extend({
     var fname = this.$("#first-name").val();
     var phone = this.$("#phone").val();
 
-    if(lname != '' && phone != ''){
-        this.collection.add({lastname: lname, firstname: fname, phone: phone});
-        this.$("#last-name").val('');
-        this.$("#first-name").val('');
-        this.$("#phone").val('');
-    }
-    else{
+    if (lname != '' && phone != '') {
+        this.collection.add({ lastname: lname, firstname: fname, phone: phone });
+        this.removeItems();
+    } else {
       alert("Please enter the good informations");
     }
   },
 
   addItem: function (contact) {
-    this.$('#contactList').append(new ContactListView({ model: contact}).render().el);
+    console.log(this);
+    this.$('#contactList').append(new ContactListView({ model: contact}).render().el, this.collection);
   },
 
   removeItems: function() {
@@ -83,5 +101,5 @@ var MainView = Backbone.View.extend({
 });
 
 
-var main_view = new MainView( {collection: this.contact_list}).render().el;
+var main_view = new MainView({ collection: this.contact_list }).render().el;
 $('#main_container').html(main_view);
